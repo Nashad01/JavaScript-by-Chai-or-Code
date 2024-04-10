00d50b1c0b3de28857b79280ec7cefc6739aaf28
Mohd Nashad Jilani <147087257+Nashad01@users.noreply.github.com>
2024-04-11 00:51:37 +0530

    Add files via upload

---
+++ 04_variables.js
@@ -0,0 +1,14 @@
+let accountEmail = "balagal@dc.com"
+let accountPassword = "292929"
+let accountCity = "London"
+let accountState
+
+console.log("accountEmail")
+
+//Another console method
+
+console.log([accountEmail, accountPassword, accountCity]);
+
+//Another console table method
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
